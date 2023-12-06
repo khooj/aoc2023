f961c89937fcc0d948d40577fa3a2a657da9063e
Igor Gilmutdinov <bladoff@gmail.com>
2023-12-07 00:22:59 +0300

    Task6 part1.

--- _6_wait_for_it/src/main.rs
+++ _6_wait_for_it/src/main.rs
@@ -32,10 +32,26 @@ fn parse_input(s: &str) -> Vec<Race> {
         .collect()
 }
 
+fn is_win_possible(time: u32, max_dist: u32, press_time: u32) -> bool {
+    debug!("time {} max_dist {} press_time {}", time, max_dist, press_time);
+    (time - press_time) * press_time > max_dist
+}
+
 fn every_possible_race_solution_part1(s: &str) -> u32 {
     let races = parse_input(s);
-    debug!("races {:?}", races);
-    1
+    let mut res = 1;
+    for race in races {
+        let mut wins = 0;
+        for press_time in 0..=race.time {
+            wins += if is_win_possible(race.time, race.dist, press_time) {
+                1
+            } else {
+                0
+            };
+        }
+        res *= if wins > 0 { wins } else { 1 };
+    }
+    res
 }
 
 fn main() {
