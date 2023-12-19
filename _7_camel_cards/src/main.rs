dd30893244a91d43782fc128e09e6f48f1017e98
Igor Gilmutdinov <bladoff@gmail.com>
2023-12-19 12:34:50 +0300

    Task7 part2.

--- _7_camel_cards/src/main.rs
+++ _7_camel_cards/src/main.rs
@@ -7,26 +7,52 @@ use std::cmp::Ordering;
 use utils::get_file_string;
 
 use std::cmp::Reverse;
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 #[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Clone)]
 enum HandStrength {
-    High = 0,
-    One,
-    Two,
-    Three,
-    Full,
-    Four,
-    Five,
+    High = 0, // 23456
+    One,      // A23A4
+    Two,      // 23432
+    Three,    // TTT98
+    Full,     // 23332
+    Four,     // AA8AA
+    Five,     // AAAAA
 }
 
 #[derive(Clone, Debug)]
 struct Hand(String, HandStrength);
 
 impl Hand {
-    fn new(s: &str) -> Hand {
+    fn old_hand(s: &str) -> Hand {
         let hand = s.to_string();
         let freq = hand.chars().counts();
+        let strength = Self::strength(&freq);
+        Hand(hand, strength)
+    }
+
+    fn new(s: &str) -> Hand {
+        let hand = s.to_string();
+        let b = hand.chars().filter(|c| *c != 'J').counts();
+        let freq_no_joker: Vec<_> = b.values().sorted().rev().collect();
+        let joker_count = hand.chars().filter(|c| *c == 'J').count();
+        let strength = Self::joker_strength(&freq_no_joker, joker_count);
+        let strength = strength.unwrap_or(Self::strength(&b));
+        Hand(hand, strength)
+    }
+
+    fn joker_strength(symb: &Vec<&usize>, jokers: usize) -> Option<HandStrength> {
+        Some(match (&symb[..], jokers) {
+            (&[], 5) | (&[1], 4) | (&[2], 3) | (&[3], 2) | (&[4], 1) => HandStrength::Five,
+            (&[1, 1], 3) | (&[3, 1], 1) | (&[2, 1], 2)=> HandStrength::Four,
+            (&[1, 1, 1], 2) | (&[2, 1, 1], 1) => HandStrength::Three,
+            (&[1, 1, 1, 1], 1) => HandStrength::One,
+            (&[2, 2], 1) => HandStrength::Full,
+            _ => return None,
+        })
+    }
+
+    fn strength(freq: &HashMap<char, usize>) -> HandStrength {
         let mut strength = HandStrength::High;
         if freq.len() == 5 {
             strength = HandStrength::High;
@@ -51,14 +77,14 @@ impl Hand {
         } else if freq.len() == 4 {
             strength = HandStrength::One;
         }
-        Hand(hand, strength)
+        strength
     }
 
     fn as_u32(c: &char) -> u32 {
         match c {
-            l @ '2'..='9' => l.to_digit(10).unwrap() - 2,
+            l @ '2'..='9' => l.to_digit(10).unwrap(),
             'T' => 10,
-            'J' => 11,
+            'J' => 1,
             'Q' => 12,
             'K' => 13,
             'A' => 14,
@@ -76,6 +102,7 @@ impl PartialEq for Hand {
         let lhs: Vec<_> = self.0.chars().collect();
         let rhs: Vec<_> = other.0.chars().collect();
         for i in 0..lhs.len() {
+            debug!("compare chars {} {}", &lhs[i], &rhs[i]);
             if let Some(k) = Hand::as_u32(&lhs[i]).partial_cmp(&Hand::as_u32(&rhs[i])) {
                 if k == Ordering::Equal {
                     continue;
@@ -106,7 +133,7 @@ impl Ord for Hand {
         let lhs: Vec<_> = self.0.chars().collect();
         let rhs: Vec<_> = other.0.chars().collect();
         for i in 0..lhs.len() {
-            // debug!("compare chars {} {}", &lhs[i], &rhs[i]);
+            debug!("compare chars {} {}", &lhs[i], &rhs[i]);
             if let Some(k) = Hand::as_u32(&lhs[i]).partial_cmp(&Hand::as_u32(&rhs[i])) {
                 if k == Ordering::Equal {
                     continue;
@@ -179,7 +206,7 @@ fn unique_elems(s: &str) -> bool {
     for b in bids {
         h.insert(b.hand.0);
     }
-    return h.len() == l
+    return h.len() == l;
 }
 
 fn total_winnings_variant2(s: &str) -> u64 {
@@ -204,7 +231,7 @@ fn main() {
 
     let s = get_file_string();
     // println!("unique_elems {}", unique_elems(&s));
-    println!("part1 {}", total_winnings(&s));
+    println!("part1 {}", total_winnings_variant2(&s));
 }
 
 #[cfg(test)]
@@ -217,13 +244,22 @@ mod tests {
 
         let hand = Hand::new("32T3K");
         assert_eq!(hand.1, HandStrength::One);
-        assert_eq!(Hand::as_u32(&'2'), 0);
-        assert_eq!(Hand::as_u32(&'9'), 7);
 
         let hand1 = Hand::new("KK677");
         let hand2 = Hand::new("KTJJT");
-        assert!(hand1 > hand2);
-        assert!(hand1 > hand);
-        assert!(hand2 > hand);
+        let hand3 = Hand::new("QQQJA");
+        assert_eq!(hand1.1, HandStrength::Two);
+        assert_eq!(hand2.1, HandStrength::Four);
+        assert_eq!(hand3.1, HandStrength::Four);
+        assert!(hand2 > hand3 && hand2 > hand1);
+        assert!(hand3 > hand1);
+
+        assert_eq!(Hand::as_u32(&'2'), 2);
+        assert_eq!(Hand::as_u32(&'J'), 1);
+        assert!(Hand::as_u32(&'2') > Hand::as_u32(&'J'));
+
+        let hand1 = Hand::new("JAAAA");
+        let hand2 = Hand::new("22222");
+        assert!(hand2 > hand1);
     }
 }
